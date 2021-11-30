efdd7097e4455a9e6ce8b7fd437f6f3a07f96020
zahidkhan262 <zahidkhan007860@gmail.com>
2021-11-30 23:46:55 +0530

    first add to cart by id

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var allData = [
-    { name: 'mango', price: 120, inCart: 0 },
     { name: 'Tomato', price: 120, inCart: 0 },
+    { name: 'mango', price: 120, inCart: 0 },
     { name: 'Orange', price: 120, inCart: 0 },
     { name: 'Burger', price: 120, inCart: 0 },
 ];
 
 
-let cartItem = document.querySelectorAll('add_cart');
+var cartBtn = document.querySelectorAll('#add_cart');
+var bindCart = document.getElementById('bind_cart');
+var cartValue = document.getElementById('cart-value');
 
-for (let i = 0; i < cartItem.length; i++) {
-    cartItem[i].addEventListener('click', function () {
-
-    })
+var a = 0;
+for (allCartId of cartBtn) {
+    allCartId.onclick = function (resp) {
+        console.log(resp)
+        a++;
+        cartValue.innerHTML = a;
+        console.log(bindCart)
+        storeData = resp.target.parentNode;
+        storeData1 = storeData.parentNode;
+        storeData2 = storeData1.parentNode;
+        storeData3 = storeData2.parentNode;
+        console.log(storeData3)
+        copyData = storeData3.cloneNode(true);
+        bindCart.appendChild(copyData)
+    }
 }
