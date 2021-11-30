e8b3eef8ca8b379b9c0680652403e49adc70def6
zahidkhan262 <zahidkhan007860@gmail.com>
2021-11-30 15:32:43 +0530

    first add to cart learning

---
+++ app.js
@@ -0,0 +1,15 @@
+var allData = [
+    { name: 'mango', price: 120, inCart: 0 },
+    { name: 'Tomato', price: 120, inCart: 0 },
+    { name: 'Orange', price: 120, inCart: 0 },
+    { name: 'Burger', price: 120, inCart: 0 },
+];
+
+
+let cartItem = document.querySelectorAll('add_cart');
+
+for (let i = 0; i < cartItem.length; i++) {
+    cartItem[i].addEventListener('click', function () {
+
+    })
+}
